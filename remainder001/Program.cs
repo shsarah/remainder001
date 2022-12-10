0a73056462e5a48bb91c01cdbd9766619858538c
shsarah <saraash@code.edu.az>
2022-12-11 00:38:40 +0400

    git branch master

--- remainder001/Program.cs
+++ remainder001/Program.cs
@@ -1,9 +1,13 @@
-﻿namespace remainder001;
+﻿namespace remainder01;
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+        Console.WriteLine("Birinci ededi daxil edin:");
+        int a = Convert.ToInt32(Console.ReadLine());
+        Console.WriteLine("Ikinci ededi daxil edin:");
+        int b = Convert.ToInt32(Console.ReadLine());
+        int c = a % b;
+        Console.WriteLine("Qaliq:" + c);
     }
 }
-
